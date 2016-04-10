833e5fb63b9c79976c7986cfe18ead8b7ae457c2
4rrow <4rrow88@gmail.com>
2016-04-11 11:20:33 +1200

    .

--- VideoRentalSystem/VideoRentalSystem/Forms/FormTemplate.cs
+++ VideoRentalSystem/VideoRentalSystem/Forms/FormTemplate.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace VideoRentalSystem.Forms
+namespace VideoRentalSystem
 {
-    public partial class FormMainMenu : VideoRentalSystem.FormTemplate
+    public partial class FormTemplate : Form
     {
-        public FormMainMenu()
+        public FormTemplate()
         {
             InitializeComponent();
         }
